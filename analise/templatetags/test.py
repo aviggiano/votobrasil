ba8bbb0bb32ded4efaf102e5f1ae3493abbeb6d3
aviggiano <antonio.viggiano@usp.br>
2014-01-06 23:35:32 -0200

    a

---
+++ analise/templatetags/test.py
@@ -0,0 +1,10 @@
+from TwitterAPI import TwitterAPI
+
+class Oauthtest:
+   'Oauth test'
+
+   def __init__(self):
+       api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
+       r = api.request('search/tweets', {'q':'pizza'})
+       for item in r.get_iterator():
+           print item
