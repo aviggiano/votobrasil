93b17d79c50b87888d05650e925dd1b41d243d2f
aviggiano <antonio.viggiano@usp.br>
2014-01-06 23:46:44 -0200

    aaa

--- analise/templatetags/test.py
+++ analise/templatetags/test.py
@@ -8,6 +8,6 @@ class Oauthtest:
 
    def __init__(self):
        api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
-       r = api.request('search/tweets', {'q':'pizza'})
+       r = api.request('search/tweets', {'q':'brasil'})
        for item in r.get_iterator():
            print item
