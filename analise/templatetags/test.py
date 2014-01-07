7762eb04076732e67872af78c69c9f451a5915c2
aviggiano <antonio.viggiano@usp.br>
2014-01-06 23:57:16 -0200

    a

--- analise/templatetags/test.py
+++ analise/templatetags/test.py
@@ -3,7 +3,7 @@ from django import template
 
 register = template.Library()
 
-def f(value):
+def t(value):
    api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
    r = api.request('search/tweets', {'q':'brasil'})
    for item in r.get_iterator():
