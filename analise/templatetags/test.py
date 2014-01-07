5a80f31c6e3e2c3e25dda4049e4c514092b914c5
aviggiano <antonio.viggiano@usp.br>
2014-01-06 23:55:17 -0200

    a

--- analise/templatetags/test.py
+++ analise/templatetags/test.py
@@ -3,17 +3,9 @@ from django import template
 
 register = template.Library()
 
-class Oauthtest:
-   'Oauth test'
-
-   def __init__(self):
-       api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
-       r = api.request('search/tweets', {'q':'brasil'})
-       for item in r.get_iterator():
-           print item
-
-   def f(value):
-       api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
-       r = api.request('search/tweets', {'q':'brasil'})
-       for item in r.get_iterator():
-           print item
+def f(value):
+   api = TwitterAPI('wHz23ocOw4SolyUBWHLqvw', 'HgY1QGTAfcxHFdLichxWlqUqgmLgOFB8QUdSnnvuY0', '66269600-HvsEP0pnInp0IgmX23LkJJBjBVwwOxbqtd9FdsrTX', '8qOKgkpF9MafSzxz2f7Fb7I207Fpj7hZ5XxE6rl2P6ykW')
+   r = api.request('search/tweets', {'q':'brasil'})
+   for item in r.get_iterator():
+      print item
+      
