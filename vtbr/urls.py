27c7a0bf52360220a5da30d5d8797af77b76c85f
aviggiano <antonio.viggiano@usp.br>
2014-01-06 23:04:09 -0200

    views

--- vtbr/urls.py
+++ vtbr/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-import contacts.views
+import analise.views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
